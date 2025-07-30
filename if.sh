fe0f857cae553956076c1c47177a715f73f731ec
jaygavhane <121130442+jaygavhane@users.noreply.github.com>
2025-07-30 22:29:47 +0530

    adding the if script

---
+++ if.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+
+userinput=$1
+cnumber=50
+
+if [ "$userinput" -gt "$cnumber" ]; then
+  echo "Guess number is bigger"
+elif [ "$userinput" -lt "$cnumber" ]; then
+  echo "Guess number is lesser"
+else
+  echo "Guess is currect"
+fi
